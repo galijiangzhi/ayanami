dfb5994a25a8d615177437e59423c6c1a217a3cf
汪贵友 <1911024051@qq.com>
2024-02-27 12:09:39 +0800

    测试图标

---
+++ ayanami/src/components/header.vue
@@ -0,0 +1,31 @@
+<!-- 该文件用于构建主页的头部导航栏 -->
+<template>
+    <div :class="header" :style=header_style>
+        <Logo />
+    </div>
+</template>
+<script>
+    import Logo from './head/logo.vue'
+    export default{
+        data(){
+            return{
+                header:"header",
+                header_style:{
+                    backgroundColor:"#181818",
+                    height: "64px",
+                    width: "100%",
+                    padding:"8px"
+                }
+            }
+        },
+        components:{
+            Logo
+        }
+    }
+</script>
+<style>
+    .header{
+        
+
+    }
+</style>
---
+++ ayanami/src/start.vue
@@ -0,0 +1,27 @@
+<script>
+import Header from './components/header.vue'
+
+export default {
+  data(){
+    return{
+      body_class:"body_div",
+      body_style:{
+        height:"100vh",
+        width:"100%",
+        backgroundColor:"#462a5b"
+      }
+    }
+  },
+  components: {
+    Header,
+}
+}
+</script>   
+
+<template>
+    <div :class="body_class" :style="body_style">
+        <Header />
+    </div>
+</template>
+<style>
+</style>
