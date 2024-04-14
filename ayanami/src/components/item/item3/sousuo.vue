944e3e5b545aa6b5abc576b3e3ccbac24d6ffacd
Your Name <you@example.com>
2024-04-14 14:57:39 +0800

    tianqi

---
+++ ayanami/src/components/item/item3/sousuo.vue
@@ -0,0 +1,65 @@
+<template>
+    <div :style="option_style">
+      <h1 :style="h1_style">{{ responseData.title }}</h1>
+      <input type="text" :style="input_st">
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        option_style: {
+          height: "40px",
+          width: "49%",
+          backgroundColor:`${this.$root.$data.color.background}`,
+          margin: "3px",
+          boxSizing: "border-box",
+          display:"flex",
+          borderRadius:"5px"
+        },
+        img_style:{
+          width:"25px",
+          height:"25px",
+          margin:"10px"
+        },
+        start_font_style:{
+          width:"25px",
+          height:"25px",
+          margin:"10px",
+          color:"#00ccb4",
+          fontSize:"25px",
+          display:"flex"
+        },
+        h1_style:{
+          fontSize:"12px",
+          height:"100%",
+          width:"70px",
+        //   background:"red",
+          wordWrap:"break-word",
+        //   flex:"0.9",
+          color:`${this.$root.$data.color.font}`,
+          border:"10x",
+          display:"flex",
+          textOverflow: "ellipsis",
+          overflow: "hidden",
+          alignItems: "center",  // 垂直居中显示
+          justifyContent: "center"
+        },
+        input_st:{
+            flex:"1",
+            height:"90%",
+            background:"rgba(255,255,255,0.3)",
+            borderRadius:"5px",
+            border:"0px"
+        },
+        responseData: {
+          title:this.url
+        },
+        isimg:true
+      };
+    },
+    props: ["url"]
+}  
+  </script>
